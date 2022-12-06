3700bf0620e8213def3edf6be35eefa49ab1d970
Bubus <100220132+Bubuz123@users.noreply.github.com>
2022-12-06 09:25:22 +0100

    Add files via upload

---
+++ IndovinaServer/src/indovinaserver/Constants.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package indovinaserver;
+
+/**
+ *
+ * @author apuzzo_cristian
+ */
+public class Constants {
+    public static final String LOGOUT = "logout";
+    public static final int PORT = 1234;
+}
---
+++ IndovinaServer/src/indovinaserver/IndovinaServer.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package indovinaserver;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author apuzzo_cristian
+ */
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class IndovinaServer {
+
+    static List<String>  parole = new ArrayList();
+    static String parola = "";
+    static int lunghezza = 0;
+    static List clients;
+    ServerSocket serverSocket;
+    static int numOfUsers = 0;
+    Socket socket;
+
+    public IndovinaServer() {
+        clients = new ArrayList<>();
+        try {
+            serverSocket = new ServerSocket(Constants.PORT);
+        } catch (IOException ex) {
+            log("Server : " + ex.getMessage());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        inizio();
+        IndovinaServer server = new IndovinaServer();
+        server.waitConnection();
+    }
+
+    private void waitConnection() throws IOException {
+        log("Server Running...");
+
+        while (true) {
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException ex) {
+                log("waitConnection : " + ex.getMessage());
+            }
+
+            log("Client accepted : " + socket.getInetAddress());
+            numOfUsers++;
+
+            ClientHandler handler = new ClientHandler(socket, "user" + numOfUsers);
+
+            Thread thread = new Thread(handler);
+            addClient(handler);
+            thread.start();
+        }
+    }
+
+    public static List<ClientHandler> getClients() {
+        return clients;
+    }
+
+    private void addClient(ClientHandler client) {
+        clients.add(client);
+    }
+
+    private void log(String message) {
+        System.out.println(message); 
+    }
+
+    public static String getParola() {
+        return parola;
+    }
+
+    private static void inizio() {
+        try {
+            String filePath = new File("Parole.txt").getAbsolutePath();
+            File myObj = new File(filePath);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                parole.add(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        int x = Util.Random(0, parole.size()-1);
+        parola = parole.get(x);
+        lunghezza = parola.length();
+    }
+}
